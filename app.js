3c5b8d45a18a52dc93f14b0b383c0a1fcf1bbdd0
mattari114 <106040687+mattari114@users.noreply.github.com>
2023-06-13 23:18:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,106 @@
-const switcher = document.querySelector('.btn');
+// 変数一覧
+var a;  // アタックフィールド1
+var b;  // アタックフィールド2
+var c;  // アタックフィールド3
+var d;  // アタックフィールド4
+var E;  // アタックフィールド5
+var f;
+var g;  // 一方的なライバル意識
+var h3, h4, h5;  // 古の力
+var i;  // エレキフィールド
+var j2;  // カミノチカラ
+var k, k3, k4;  // カリスマ
+var l;
+var m;  // 逆境
+var n3, n4;  // クリティカル1
+var o3, o4;  // クリティカル2
+var p3, p4;  // クリティカル3
+var q3, q4;  // クリティカル4
+var r;  // 剣帝陣
+var s;  // 鼓舞
+var t2, t3, t4;  // 闘神
+var u;  // 同族嫌悪
+var v;  // 同調圧力
+var w;  // 腹太鼓
+var x;  // 働きたくないよー
+var y3, y4;  // 必殺クリティカル1
+var z3, z4;  // 必殺クリティカル2
+var aa3, aa4;  // 必殺クリティカル3
+var bb3, bb4;  // 必殺クリティカル4
+var cc3, cc4;  // 必殺クリティカル5
+var dd2;  // マサムネ
+var ee;  // 群れとしての強さ
+var ff;  // ラスト・サムライ
+var gg2, gg3, gg4, gg5;  // 天照の槍
+var hh2, hh5;  // 桜華紅姫
+var ii3, ii4, ii5;  // ギガシンカ
+var jj, jj3, jj4;  // 自由への翼
+var kk5;  // 主人公
+var ll2, ll3, ll4, ll5;  // 真・天照の槍
+var mm2, mm3;  // ダンボール
+var nn2;  // 血の紋章
+var oo2;  // ツイン眼帯
+var pp5;  // 火竜の槍
+var qq2, qq3, qq4;  // 北斗七星
+var rr;  // 神風
+var ss, ss3, ss4;  // 聖剣の誓い3
+var tt, tt3, tt4;  // 聖剣の誓い4
+var uu;  // 力の陣3
+var vv;  // 力の陣4
+var ww, ww3, ww4;  // ティロ支援3
+var xx, xx3, xx4;  // ティロ支援4
+var yy, yy3, yy4;  // ビルド強化3
+var zz, zz3, zz4;  // ビルド強化4
+var aaa;  // ジェネ眼
+var bbb;  // みん違
+var ccc3;  // アンドロイドの刀
+var qqq;  // 大見せ場
+var rrr2;  // あかい目
+var sss2;  // 攻撃up1
+var ttt2;  // 2
+var uuu2;  // 3
+var vvv2;  // 攻撃up4
+var www;  // 紋所
+var xxx, xxx3, xxx4;  // 盾聖剣3
+var yyy, yyy3, yyy4;  // 盾聖剣4
+var zzz;  // 栄光の槍
+var aaaa, aaaa3, aaaa4;  // クロス支援3
+var bbbb, bbbb3, bbbb4;  // クロス支援4
+var cccc, cccc3, cccc4;  // 十色
+var dddd5;  // 疲労
+var eeee;  // 嫌われ
+var ffff, ffff3, ffff4;  // ナイフ
+var gggg;  // 窮鼠
 
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
+var ddd5;  // ギガシン高速
+var fff5;  // 天照速度
+var ggg5;  // 真天照速度
+var hhh5;  // 攻撃速度1
+var iii5;  // 2
+var jjj5;  // 3
+var kkk5;  // 4
 
-    const className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
+var lll;  // 任意の通常加算
+var mmm2;  // 任意の元ステ
+var nnn3;  // 任意のクリ
+var ooo4;  // 任意の倍率
+var ppp5;  // 任意の独立
+var talent;  // 任意の才能値
+var level;  // 任意のレベル
 
-    console.log('current class name: ' + className);
-});
+var ini;  // 初期火力
+
+var motoatk;  // 元ステ
+var motoatk2;  // 強化後の元ステ
+var exatk;  // 初期火力と元ステ
+var atk;  // 通常
+var cri;  // クリ率
+var crid;  // 倍率
+var other;  // 独立
+var fix;  // 固定ダメ
+
+var finish;  // 算出
+
+var hidari;  // 割り算の左
+var migi;  // 割り算の右
+var finish2;  // 火力同士の割り算算出
